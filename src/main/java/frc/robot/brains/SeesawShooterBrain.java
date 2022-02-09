8e44a30944782c19173e22a8be1df2fc8b2e098c
Nikki <chunn.22@stu.materdei.org>
2022-02-08 16:55:51 -0800

    Create brain for SeesawShooter

---
+++ src/main/java/frc/robot/brains/SeesawShooterBrain.java
@@ -0,0 +1,26 @@
+
+package frc.robot.brains;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+
+// This class contains all the shared NetworkTableEntries for the DiffDriver Subsystem,
+// their default values, and methods for retrieving their current values.
+public class SeesawShooterBrain {
+
+    //----------------//
+    // Default Values //
+    //----------------//
+
+
+    //---------------------//
+    // NetworkTableEntries //
+    //---------------------//
+
+    //---------//
+    // Setters //
+    //---------//
+
+    //---------//
+    // Getters //
+    //---------//
+}
