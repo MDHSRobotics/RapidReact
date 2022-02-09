270feba484b013af9a11e81b78240d3b9c9141a4
Nathan <han.22@stu.materdei.org>
2022-02-08 16:31:41 -0800

    fixed CloseClaw

--- src/main/java/frc/robot/commands/PickUp/CloseClaw.java
+++ src/main/java/frc/robot/commands/PickUp/CloseClaw.java
@@ -1,18 +1,19 @@
-
-package frc.robot.commands.lighter;
+package frc.robot.commands.PickUp;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.Timer;
 
 import frc.robot.consoles.Logger;
-import frc.robot.subsystems.Lighter;
+//import frc.robot.subsystems.Lighter;
+import frc.robot.subsystems.PickerUpper;
 
 
 public class CloseClaw extends CommandBase {
 
+    private PickerUpper m_pickup;
+
     // TODO: Make these constructor parameters. Use overloading for these default values.
 
-    public CloseClaw(PickerUpper pickUp) {
+    public CloseClaw(PickerUpper pickup) {
         Logger.setup("Constructing Command: CloseClaw...");
 
         // Add given subsystem requirements
@@ -29,13 +30,13 @@ public class CloseClaw extends CommandBase {
     //real code
     @Override
     public void execute() {
-       m_pickup.openHorizontal();
+       m_pickup.closeHorizontal();
     }
 
     // This command continues until it cycles through the set number of cycles
     @Override
     public boolean isFinished() {
-        
+        return false;
     }
 
     @Override
